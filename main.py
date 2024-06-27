7acddb5a4192a71662708a41d1cf0ceac8f736a9
João Pablo <jp.pablolima@gmail.com>
2024-06-27 07:34:37 -0300

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Olá, mundo")
+#!/usr/bin/env python3
+
+import requests 
+
+url = requests.get("https://www.google.com")
+
+if x.status_code == 200:
+    print("Ok")
+else:
+    print("Error!")
