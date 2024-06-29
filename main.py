22c25aa52d1be1b6ccf3205fd36cf0c279d25dc4
João Pablo <jp.pablolima@gmail.com>
2024-06-29 06:17:06 -0300

    update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-import requests 
+import requests
 
-url = requests.get("https://www.google.com")
-
-if x.status_code == 200:
-    print("Ok")
-else:
-    print("Error!")
+try:
+    response = requests.get("https://www.google.com")
+    if response.status_code == 200:
+        print("Ok")
+    else:
+        print("Error!")
+except requests.exceptions.RequestException as e:
+    print(f"Error: {e}")
